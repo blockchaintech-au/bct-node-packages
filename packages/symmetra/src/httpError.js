ae7071a2fdd433d16bb98a769003ab142af390c0
Shu Hui <dreamlock0630@gmail.com>
2018-07-03 12:17:39 +1000

    fix error handling bug: add data in Error Object

--- packages/symmetra/src/httpError.js
+++ packages/symmetra/src/httpError.js
@@ -1,7 +1,11 @@
 class HttpError extends Error {
   constructor(error) {
     super(error.message);
-    this.status = error.response ? error.response.status : 500;
+    if (error.response) {
+      this.status = error.response.status;
+      this.data = error.response.data;
+    }
+    this.status = 500;
   }
 }
 
