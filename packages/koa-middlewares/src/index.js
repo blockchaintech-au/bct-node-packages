db4520cc718ff0a82d6822a3ee36a629bd645c22
Shu Hui <dreamlock0630@gmail.com>
2018-07-14 22:08:44 +1000

    1

--- packages/koa-middlewares/src/index.js
+++ packages/koa-middlewares/src/index.js
@@ -1,11 +1,12 @@
 import authorize from './middlewares/authorize';
 import headerHandler from './middlewares/header-handler';
 import errorHandler from './middlewares/error-handler';
-import configs from './configs';
+import { JWT_SECRET, TOKEN_EXPIRATION_TIME } from './configs';
 
 export {
   authorize,
   headerHandler,
   errorHandler,
-  configs,
+  JWT_SECRET,
+  TOKEN_EXPIRATION_TIME,
 };
