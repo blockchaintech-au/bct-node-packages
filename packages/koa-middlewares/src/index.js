275d7fe5222c4f5b8d549915fa841ade91af4d94
Shu Hui <dreamlock0630@gmail.com>
2018-07-14 21:59:05 +1000

    update npm tokens

--- packages/koa-middlewares/src/index.js
+++ packages/koa-middlewares/src/index.js
@@ -1,9 +1,11 @@
 import authorize from './middlewares/authorize';
 import headerHandler from './middlewares/header-handler';
 import errorHandler from './middlewares/error-handler';
+import configs from './configs';
 
 export {
   authorize,
   headerHandler,
   errorHandler,
+  configs,
 };
